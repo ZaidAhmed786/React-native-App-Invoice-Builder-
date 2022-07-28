f324250a91e5f078a7d3a9dfa03613029be84578
Zaid Ahmed <km.zaidshahid@gmail.com>
2022-07-28 20:40:50 +0500

    8:40 28/07/2022

--- src/BillCreation.js
+++ src/BillCreation.js
@@ -1,15 +1,35 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 import Data from "../src/Data"
+import {Picker} from '@react-native-picker/picker';
+import RNPickerSelect from "react-native-picker-select";
 
 const BillCreation = () => {
+  const [name, setName] = useState("")
+  const [MobileNumber, setMobileNumber] = useState("")
+  const [Address, setAddress] = useState("")
+  const [Product, setProduct] = useState("A")
+  const [Quantity, setQuantity] = useState("")
+  // const onChangeText = (text)=>{
+  //   setName(text)
+  // }
   return (
     <View style={styles.Container}>
       <ScrollView>
         <View>
-            <Data placeholder="Name"/>
-            <Data placeholder="Number"/>
-            <Data />
+            <Data title="Name" placeholder="Name" value={name} onChangeText={(text)=>{setName(text)}}/>
+            <Data title="Mobile Number" placeholder="Number" keyboardType="number-pad" value={MobileNumber} onChangeText={(text)=>{setMobileNumber(text)}}/>
+            <Data title="Address" placeholder="Address" value={Address} onChangeText={(text)=>{setAddress(text)}}/>
+            <Data title="Product" placeholder="Product" value={Product} onChangeText={(text)=>{setProduct(text)}}/>
+            <RNPickerSelect
+            onValueChange={(value) => console.log(value)}
+            items={[
+                { label: 'Football', value: 'football' },
+                { label: 'Baseball', value: 'baseball' },
+                { label: 'Hockey', value: 'hockey' },
+            ]} />
+
+            <Data title="Quantity" placeholder="Quantity" value={Quantity} onChangeText={(text)=>{setQuantity(text)}}/>
         </View>
       </ScrollView>
     </View>
--- src/Data.js
+++ src/Data.js
@@ -4,9 +4,12 @@ import React from 'react'
 const Name = (props) => {
   return (
     <View style={styles.InputContainer}>
-      <Text>Name</Text>
+      <Text>{props.title}</Text>
       <TextInput
       placeholder={props.placeholder}
+      value={props.value}
+      onChangeText={props.onChangeText}
+      keyboardType={props.keyboardType}
       style={styles.TextInput}
       />
     </View>
